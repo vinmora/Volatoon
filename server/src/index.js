5f6a156242697dcb420fc0d34a529042555dcfb4
Prima Hagantha <81164903+primahagantha@users.noreply.github.com>
2024-11-30 16:42:00 +0700

    V

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import authController from "./auth/auth.controller.js"
-import userController from "./user/user.controller.js"
-import bookmarkController from "./bookmark/bookmark.controller.js"
-import commentController from "./comment/comment.controller.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authController from "./auth/auth.controller.js";
+import userController from "./user/user.controller.js";
+import bookmarkController from "./bookmark/bookmark.controller.js";
+import commentController from "./comment/comment.controller.js";
 
 // import unknownEndPoint from './middleware/unknownEndpoint.js';
-const app = express()
+const app = express();
 
-dotenv.config()
-const port = 2000
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+const port = 2000;
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Volatoon api!')
-})
+app.get("/", (req, res) => {
+  res.send("Volatoon api!");
+});
 
-app.use('/api/auth/', authController)
-app.use('/api/', userController)
-app.use('/api/', bookmarkController)
-app.use('/api/', commentController)
+app.use("/api/auth/", authController);
+app.use("/api/", userController);
+app.use("/api/", bookmarkController);
+app.use("/api/", commentController);
 
 app.listen(port, () => {
-  console.log(`BreadFinance is listening on port ${port}`)
-})
+  console.log(`Volatoon is listening on port ${port}`);
+});
